8ff9da39425eab8ad546711cda72474673759efd
Void <guedznfs@gmail.com>
2024-08-30 17:50:45 -0300

    Removed card and noticia components

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { CardComponent } from "../../components/card/card.component";
 import {RouterModule} from '@angular/router';
 import { MenuBarraComponent } from "../../components/menu-barra/menu-barra.component";
 import { JogoCardComponent } from "../../components/jogo-card/jogo-card.component";
@@ -7,7 +6,7 @@ import { JogoCardComponent } from "../../components/jogo-card/jogo-card.componen
 @Component({
   selector: 'page-inicio',
   standalone: true,
-  imports: [CardComponent, RouterModule, MenuBarraComponent, JogoCardComponent],
+  imports: [RouterModule, MenuBarraComponent, JogoCardComponent],
   templateUrl: './inicio.page.html',
   styleUrl: './inicio.page.css'
 })
